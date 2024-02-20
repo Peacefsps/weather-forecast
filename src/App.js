42feb85b1f29eeeb1df068cabc010bb9f43f8e41
Peace Oyewole <oyewolepeace94@gmail.com>
2024-02-20 10:29:17 -1100

    Created weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by 
+          <a href="https://github.com/Peacefsps"> Peace Iwakin </a>
+          and is 
+          <a href="#"> open-sourced on Github.</a>
+        </footer>
+      </div>
     </div>
   );
 }
